25398a83a3ac46726a40141b7b0bd1624f6c3d35
Srdjan Simic <srdjan_kralj_97@live.com>
2020-04-14 22:56:23 +0200

    filter

--- vue-cli/veujs-playlist/src/main.js
+++ vue-cli/veujs-playlist/src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+//import VueResource from 'vue-resource'
 /*import Ninjas from './ninjas.vue' //globaly registration
 
 Vue.component('ninjas',Ninjas); //component global registration
 */
+//Vue.use(VueResource);
+Vue.fillter('to-upper',function(value){
+  return value.toUpperCase();
+});
 
 export const bus = new Vue();
 Vue.directive('rainbow',{
@@ -13,7 +18,7 @@ Vue.directive('rainbow',{
 
         }
         if(binding.arg == 'column'){
-          
+
         }
 
     }
