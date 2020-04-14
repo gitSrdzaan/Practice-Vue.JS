c16ecf4ea9c1da92dd11d2660470b8ff1e033ee3
Srdjan Simic <srdjan_kralj_97@live.com>
2020-04-14 10:59:58 +0200

    events<click><dblclick><mousemove>

--- app.js
+++ app.js
@@ -5,13 +5,27 @@ new Vue({
         name :'Srdjan',
         job : 'Programer',
         website : 'https://www.google.ba',
-        websiteTag : '<a href="https://www.google.ba">WebisteTag</a>'
+        websiteTag : '<a href="https://www.google.ba">WebisteTag</a>',
+        age : 23,
+        x:0,
+        y:0
     },
     methods:{
         greet : function(time){
             //this.name 
             //this.job
             return 'Good '+time + ' '+ this.name
+        },
+        addAge : function(inc){
+             this.age+=inc;
+        },
+        subAge : function(dec){
+             this.age-=dec;
+        },
+        coordinates:function(event){
+            //console.log(event);
+            this.x = event.offsetX;
+            this.y = event.offsetY;
         }
     }
 
